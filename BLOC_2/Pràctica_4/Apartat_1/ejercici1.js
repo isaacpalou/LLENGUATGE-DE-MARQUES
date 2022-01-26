0e9dbb065b2092ce5ea58e96b2ffedad1ac5aaa2
Isaac Palou <_palougijon@cifpfbmoll.eu>
2022-01-26 13:46:54 +0100

    feat: :sparkles: Iniciar nueva práctica

---
+++ BLOC_2/Pràctica_4/Apartat_1/ejercici1.js
@@ -0,0 +1,11 @@
+function ejercicio1(){
+    const title = document.getElementsByTagName("h2");
+    const paragraph = document.getElementsByTagName("p");
+
+    for (index = 0; index < title.length; index++) {
+        window.alert(title[index].innerHTML)
+    }
+    for(index = 0; index < paragraph.length; index++){
+        window.alert(paragraph[index].innerHTML)
+    }
+}
