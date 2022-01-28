853a04994af0fed6a8e3f1f123e8e41a462f50fc
Isaac <_palougijon@cifpfbmoll.eu>
2022-01-28 16:48:11 +0100

    feat: :sparkles: Añadir actividades resueltas

--- BLOC_2/Pràctica_4/Apartat_1/scripts.js
+++ BLOC_2/Pràctica_4/Apartat_1/scripts.js
@@ -9,3 +9,38 @@ function ejercicio1() {
         window.alert(paragraph[index].innerHTML)
     }
 }
+
+function ejercicio2(){
+    //Le Pides un nuevo subtítulo en una pestaña emergente.
+    const newsubtitle = prompt("Inserte el nuevo h3");
+    //Asignas a la variable subtitle todos los elementos con la ID "e2"
+    const subtitle = document.getElementById("e2");
+    //Ahora cambias el valor de esos elementos por el valor pedido.
+    subtitle.innerHTML = newsubtitle
+}
+
+function ejercicio3(){
+    var p = document.getElementsByTagName("div");
+    var fill = p[0].firstChild;
+    p[0].removeChild(fill);
+}
+
+function ejercicio4(){
+    var x = parseInt(
+        prompt("Indica valor del tamaño")
+    );
+    document.getElementById("link").style.fontSize = x + "em";
+}
+
+function ejercicio5(){
+    for (let i = 0; i < 2; i++) {
+        var text_color = prompt("Color del texto del párrafo" + (i + 1) + ": ");
+        var background_color = prompt("Color del fondo del párrafo" + (i + 1) + ": ");
+        document.getElementsByTagName("p")[i].style.color = text_color;
+        document.getElementsByTagName("p")[i].style.background = background_color;
+    }
+}
+
+function ejercicio6(){
+    
+}
