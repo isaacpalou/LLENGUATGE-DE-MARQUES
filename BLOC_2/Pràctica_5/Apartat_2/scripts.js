7e79e60e7c290472b02e11bbb90bb6104ac90084
Isaac Palou <_palougijon@cifpfbmoll.eu>
2022-02-01 10:39:21 +0100

    feat: :sparkles: Acabar actividad 3 Practica 5

--- BLOC_2/Pràctica_5/Apartat_2/scripts.js
+++ BLOC_2/Pràctica_5/Apartat_2/scripts.js
@@ -1,9 +1,3 @@
 function cambiar_section(){
-    var seccion = document.getElementsByClassName("seccion1")
-    console.log(seccion)
-    
-    seccion.classListreplace("seccion1", "format")
-}
-function alerta(){
-    alert("chupame la polla")
+    document.getElementById("myDIV").classList.toggle("format")
 }
